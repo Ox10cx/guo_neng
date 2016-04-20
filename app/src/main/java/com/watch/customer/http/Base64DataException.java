87a811877e52deb2528a1aa5aa2899547ff856be
qinjiangwei <qinjiangwei@uascent.com>
2016-04-20 18:31:45 +0800

    wifi domo

--- app/src/main/java/com/watch/customer/http/Base64DataException.java
+++
@@ -1,9 +0,0 @@
-package com.watch.customer.http;
-
-import java.io.IOException;
-
-public class Base64DataException extends IOException {
-    public Base64DataException(String detailMessage) {
-        super(detailMessage);
-    }
-}
